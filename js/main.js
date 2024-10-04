b635fc3bb2ce17574fcea799ee52aa6ced5aae76
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-10-05 00:35:39 +0500

    the end

--- js/main.js
+++ js/main.js
@@ -12,3 +12,34 @@ if (rangeInputs.length) {
         inp.addEventListener('input', fnc)
     })
 }
+
+let media_menu = document.querySelector('.media_menu');
+let burger = document.querySelector('.burger');
+let body = document.querySelector('.body');
+let close = document.querySelector('.close');
+let open = document.querySelector('.open');
+
+burger.addEventListener('click', () => {
+    media_menu.classList.toggle('hidden');
+    body.classList.toggle('overflow-hidden');
+    open.classList.toggle('hidden');
+    close.classList.toggle('hidden');
+
+})
+
+
+let swiper = new Swiper(".bankSwiper", {
+    spaceBetween: 12,
+    slidesPerView: 1.2,
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1280: {
+            slidesPerView: 5,
+        },
+    },
+});
